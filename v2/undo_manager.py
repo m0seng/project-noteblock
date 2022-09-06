baca981b6cb1e80234f7fcc0ed8b77763eefaf31
Moses Ng <ngsunghim@hotmail.com>
2022-09-06 11:49:35 +0100

    actually event should be in the save mixin

--- v2/undo_manager.py
+++ v2/undo_manager.py
@@ -1,6 +1,5 @@
 from collections import deque
 from save_mixin import SaveMixin
-from event import Event
 
 class Transaction:
     def begin(self, obj: SaveMixin):
@@ -22,7 +21,6 @@ class UndoManager:
         self.future_length = 5
         self.past: deque[Transaction] = deque(maxlen=self.past_length)
         self.future: deque[Transaction] = deque(maxlen=self.future_length)
-        self.event: Event = Event()
 
     def begin_transaction(self, obj: SaveMixin):
         self.trans = Transaction()
@@ -37,10 +35,8 @@ class UndoManager:
         trans = self.past.pop()
         trans.undo()
         self.future.append(trans)
-        self.event.trigger()
 
     def redo(self):
         trans = self.future.pop()
         trans.redo()
         self.past.append(trans)
-        self.event.trigger()
