9afeb0b30447d3926c1c61d58b455fd0bc6869d9
Moses Ng <ngsunghim@hotmail.com>
2022-09-27 11:46:06 +0100

    gui things begin

---
+++ v3/listener_frame.py
@@ -0,0 +1,30 @@
+from abc import ABC, abstractmethod
+import tkinter as tk
+import tkinter.ttk as ttk
+from .save_mixin import SaveMixin
+
+class ListenerFrame(ttk.Frame, ABC):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.model: SaveMixin = None
+        self.block_sync = False
+
+    @abstractmethod
+    def sync(self):
+        ...
+
+    def callback(self):
+        if self.model is not None and not self.block_sync:
+            self.sync()
+
+    def connect(self, model: SaveMixin):
+        if self.model is not None:
+            self.disconnect()
+        model.change_event.add_listener(self.callback)
+        self.model = model
+        self.sync()
+
+    def disconnect(self):
+        if self.model is not None:
+            self.model.change_event.remove_listener(self.callback)
+            self.model = None
