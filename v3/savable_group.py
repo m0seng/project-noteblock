02b90aae1ea19974d47f27a14871ef1109a0209b
Moses Ng <ngsunghim@hotmail.com>
2022-09-29 12:27:34 +0100

    ok sejacket

--- v3/savable_group.py
+++ v3/savable_group.py
@@ -1,15 +1,24 @@
 from .save_mixin import SaveMixin
+from abc import ABC, abstractmethod
+from typing import Type
 
 class SavableGroup(SaveMixin):
-    def __init__(self, *args, **kwargs):
+    def __init__(self, subclass: Type[SaveMixin], *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.savables: list[SaveMixin] = []
+        self.savables: list[subclass] = []
+        self.subclass = subclass
 
     def to_dict(self):
         return {
-            "savables": savable.to_dict() for savable in self.savables
+            "savables": [savable.to_dict() for savable in self.savables]
         }
 
     def from_dict(self, source):
+        self.savables = []
         for saved_dict in source["savables"]:
-            ... # TODO
+            savable = self.subclass.create_from_dict(saved_dict)
+            self.savables.append(savable)
+
+    @abstractmethod
+    def init_subclass(self, source) -> SaveMixin:
+        ...
