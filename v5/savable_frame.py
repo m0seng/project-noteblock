368c6db17311f19683ccd7f4dfee9c0740af3b9f
Moses Ng <ngsunghim@hotmail.com>
2022-10-23 17:20:22 +0100

    pianoroll is in progress

--- v5/savable_frame.py
+++ v5/savable_frame.py
@@ -6,27 +6,24 @@ from . import events
 
 # TODO: HOW TO ATTACH TO MULTIPLE THINGS?
 # e.g.: Piano Roll attaches to Pattern, PatternGroup, Config and Preferences
+# solution: model is a loose coupling
+# just hook into the events and run custom class/object checks
 
 class SavableFrame(ttk.Frame, ABC):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    def __init__(self, parent, *args, **kwargs):
+        super().__init__(parent, *args, **kwargs)
         self.model: Savable = None
         self.suppress_sync = False # use when this Frame is making the edits
         events.edit.add_listener(self.update)
 
     @abstractmethod
-    def sync(self):
-        ...
-
     def update(self, sender):
-        if sender is self.model and not self.suppress_sync:
-            self.sync()
+        ...
 
     def connect(self, model: Savable):
         if self.model is not None:
             self.disconnect()
         self.model = model
-        self.sync()
 
     def disconnect(self):
         self.model = None
