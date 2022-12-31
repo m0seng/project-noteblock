699ac70e2dfad86f6785fb2ec08484014e705a6d
Moses Ng <ngsunghim@hotmail.com>
2022-12-31 16:14:02 +0000

    added group depth to undo manager

--- vx/undo_manager.py
+++ vx/undo_manager.py
@@ -6,41 +6,40 @@ class UndoManager:
         self.past: deque[Action | list[Action]] = deque(maxlen=past_len)
         self.future: deque[Action | list[Action]] = deque(maxlen=future_len)
         self.current_group: list[Action] = []
-        self.making_group: bool = False
+        self.group_depth: int = 0
 
     def reset(self):
         self.past.clear()
         self.future.clear()
         self.current_group = []
-        self.making_group = False
+        self.group_depth = 0
 
     def perform_without_undo(self, new_action: Action):
         new_action.perform()
 
     def perform(self, new_action: Action):
         new_action.perform()
-        if self.making_group:
+        if self.group_depth > 0:
             self.current_group.append(new_action)
         else:
             self.past.append(new_action)
         self.future.clear()
     
     def start_group(self):
-        if not self.making_group:
-            self.current_group = []
-            self.making_group = True
+        self.current_group = []
+        self.group_depth += 1
 
     def end_group(self):
-        if self.making_group:
+        if self.group_depth > 0:
             self.past.append(self.current_group)
             self.current_group = []
-            self.making_group = False
+            self.group_depth -= 1
 
     def can_undo(self):
         return len(self.past) > 0
 
     def undo(self):
-        if self.can_undo() and not self.making_group:
+        if self.can_undo() and self.group_depth == 0:
             action = self.past.pop()
             if isinstance(action, list):
                 for act in reversed(action): act.undo()
@@ -52,7 +51,7 @@ class UndoManager:
         return len(self.future) > 0
 
     def redo(self):
-        if self.can_redo() and not self.making_group:
+        if self.can_redo() and self.group_depth == 0:
             action = self.future.pop()
             if isinstance(action, list):
                 for act in action: act.perform()
