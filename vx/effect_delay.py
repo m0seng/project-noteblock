ef6c98354dacee4622b16faddb5b91afe6657a31
Moses Ng <ngsunghim@hotmail.com>
2023-01-01 06:58:35 +0000

    me when i fix the fix

--- vx/effect_delay.py
+++ vx/effect_delay.py
@@ -10,7 +10,7 @@ class EffectDelay(Effect):
         self._set_property("dry_mix", 1.0)
         self._set_property("wet_mix", 0.5)
         self._set_property("wet_pan", 0.0)
-        self.notes_buffer: list[list[Note]] = [None]
+        self.notes_buffer: list[list[Note]] = [[]]
         self.buffer_index: int = 0
 
     def process_notes(self, notes: list[Note], mono_tick: int) -> list[Note]:
@@ -32,7 +32,7 @@ class EffectDelay(Effect):
         if self.buffer_index > self.get_property("delay_ticks"): # loop back round
             self.buffer_index = 0
         elif self.buffer_index > len(self.notes_buffer): # extend buffer
-            self.notes_buffer.append(None)
+            self.notes_buffer.append([])
 
         # output
         return dry_notes + wet_notes
